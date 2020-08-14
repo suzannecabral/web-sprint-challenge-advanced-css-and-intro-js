82d63fb415e5e34c75c89bb69a536c656bacbc84
Suzanne Cabral <suzannecabral@live.com>
2020-08-14 10:01:24 -0700

    Completed JS 3 - getArtistByIndex

--- index.js
+++ index.js
@@ -236,13 +236,15 @@ fixName(artists,8,"Vincent van Gogh");
 console.log("-------------#3: getArtistsByIndex -------------");
 
 function getArtistByIndex(array, index) {
-    /* code here */
+    console.log(`The artist at index ${index} is ${array[index].name}`);
   }
   
-  /**
-
+getArtistByIndex(artists,0);
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
+
+console.log("-------------#4: get20s -------------");
+
 
 function get20s(/* Code here */){
 
